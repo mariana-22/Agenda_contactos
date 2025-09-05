8cadfb3c3673ec521d78e82a3e43461d3d017e78
Cielorodr <skylbr2712@gmail.com>
2025-09-05 12:24:06 -0500

    creacion de la clase contacto y sus atributos con el tipo de variable

--- Mariana.ts
+++ Mariana.ts
@@ -4,6 +4,21 @@
     type telefono=number
     type email=string
 
+class contacto {
+        id:id;
+        nombre:nombre;
+        telefono:telefono;
+        email:email;
+
+    constructor(id:id, nombre:nombre, telefono:telefono, email:email){
+        this.id=id;
+        this.nombre=nombre;
+        this.telefono=telefono;
+        this.email=email;
+        
+    }
+    
+}
 
 
 
